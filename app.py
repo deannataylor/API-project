1c8221716174eb44392dee945938494669e45183
deannataylor <dmtaylor@gmail.com>
2014-11-10 12:34:34 -0500

    html

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app=Flask(__name__)
 def index():
     return render_template("home.html")
 
+@app.route("/movies")
+def movies():
+    return render_template("results.html")
+
 if __name__=="__main__":
     app.debug=True
-    app.run(host="0.0.0.0",port=8000)
+    app.run()
