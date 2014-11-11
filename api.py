30567a60b4d31d3a09f4904920e592d0ada19397
Sophie Gershon <sophgersh@gmail.com>
2014-11-11 18:29:06 -0500

    NY Times API started to implement

--- api.py
+++ api.py
@@ -1,17 +1,28 @@
-import urllib2
-import json
+import urllib2, json
 
-url="http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=au5nf6pd9fwwh4kjbqqt6jws&q=%s"
-key = "au5nf6pd9fwwh4kjbqqt6jws"
+t_url="http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=au5nf6pd9fwwh4kjbqqt6jws&q=%s"
+article_url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=%s&end_date=%s&api-key=c45e227370aff9e7405b5f40db5249cf:12:70174992"
 
-def getStuff(query):
-    request = urllib2.urlopen(url % (query))
+
+def getMovie(query):
+    request = urllib2.urlopen(t_url % (query))
     result = request.read()
     d = json.loads(result)
-    print d["total"]
-    movies = d["movies"] 
-    for m in movies:
-        print m["title"]
+    return d["movie"][0]  # returns first movie that gets returned 
+
+def getDate(movie):
+    return movie['relase_dates']['theater']
+
+def getArticles(date):
+    '''
+    date: in the form YYYYMMDD
+    '''
+    request = urllib2.urlopen(article_url % (date,date))
+    result = request.read()
+    d = json.loads(result)
+    articles = d['response']['docs']
+    for i in range(5):
+        print articles[i]['headline']['main']
 
 def search(query):
     request = urllib2.urlopen(url % (query))
@@ -23,16 +34,29 @@ def search(query):
             if a["name"] == query:
                 print url
 
+
 if __name__ == "__main__":
-    print url
-    search("Toy Story 3")
-    
+    #getStuff("Toy+Story+3")
+    getArticles(20141111)
 
 """
+TOMATOES:
 names of stuff:
 "title"
 "ratings"["critics_score","audience_score"]
 "abridged_cast" ["name": , "characters ]
 """
 
+"""
+NYTIMES ARTICLES: 
+articles_url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=%s&end_date=%s]&api-key=c45e227370aff9e7405b5f40db5249cf:12:70174992"
 
+under fq: &fq=news_desk:("Sports" "Foreign")
+    pub_date	Timestamp (YYYY-MM-DD)
+    news_desk	Single token
+    news_desk.contains	Multiple tokens
+
+begin_date = YYYYMMDD
+end_date = YYYYMMDD
+
+"""
