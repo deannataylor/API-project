77ec2e931ce48c64e583f1670ab524dfa431bdf1
Sophie Gershon <sophgersh@gmail.com>
2014-11-11 21:26:44 -0500

    hyperlink

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask,request,url_for,redirect,render_template
+from flask import flash, Flask,request,url_for,redirect,render_template
 import urllib2, json, api
 
 app=Flask(__name__)
+app.secret_key = 'a'
+
 @app.route("/")
 def index():  
     return render_template("home.html")
 
-@app.route("/movies", methods=['POST'])
+@app.route("/movies", methods=['POST','GET'])
 def movies():
     if request.method == 'POST':
         movie=request.form["movie"]
@@ -14,7 +16,7 @@ def movies():
         return render_template("results.html",articles=articles)
     else:
         flash("Enter a movie")
-        redirect('/')
+        return redirect('/')
 
 if __name__=="__main__":
     app.debug=True
