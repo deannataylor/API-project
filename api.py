18247689a408d708e37b7da3bbbd15381fd080fe
Sophie Gershon <sophgersh@gmail.com>
2014-11-11 19:44:14 -0500

    formatting url

--- api.py
+++ api.py
@@ -1,12 +1,12 @@
 import urllib2, json
 
 t_url="http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=au5nf6pd9fwwh4kjbqqt6jws&q=%s"
-article_url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?&fq=pub_date:(%s)&api-key=c45e227370aff9e7405b5f40db5249cf:12:70174992"
-
+article_url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?&fq=pub_date:(%s) AND news_desk:(\"Movies\")&api-key=c45e227370aff9e7405b5f40db5249cf:12:70174992"
+a = "http://api.nytimes.com/svc/search/v2/articlesearch.json?&fq=pub_date:(%s)&api-key=c45e227370aff9e7405b5f40db5249cf:12:70174992"
 
 def getMovie(query):
     '''
-    query: words+words+
+    query: words+words
     returns: YYYY-MM-DD
     '''
     request = urllib2.urlopen(t_url % (query))
@@ -21,12 +21,16 @@ def getArticles(date):
     '''
     date: in the form YYYY-MM-DD
     '''
-    request = urllib2.urlopen(article_url % (date,date))
+    #request = urllib2.urlopen(article_url % (date))
+    
+    request = urllib2.urlopen(a % (date))
     result = request.read()
     d = json.loads(result)
     articles = d['response']['docs']
-    for i in range(5):
+    i = 0
+    while i < len(articles) and i < 5:
         print articles[i]['headline']['main']
+        i+=1
 
 def search(query):
     request = urllib2.urlopen(url % (query))
@@ -42,8 +46,9 @@ def search(query):
 if __name__ == "__main__":
 
     #getStuff("Toy+Story+3")
-    getArticles(20141111)
-
+    getArticles(2014-11-11)
+    #a = a % ("2014-11-11", "Sports")
+    
     
 
 """
@@ -56,7 +61,7 @@ names of stuff:
 
 """
 NYTIMES ARTICLES: 
-articles_url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=%s&end_date=%s]&api-key=c45e227370aff9e7405b5f40db5249cf:12:70174992"
+http://api.nytimes.com/svc/search/v2/articlesearch.json?&fq=pub_date:(2014-11-11)%20AND%20news_desk:(%22Movies%22)&api-key=c45e227370aff9e7405b5f40db5249cf:12:70174992
 
 under fq: &fq=news_desk:("Sports" "Foreign")
     pub_date	Timestamp (YYYY-MM-DD)
